d8d0dc472826508e52ad298012ed8a390c83b9c0
vii <vii@divero.net>
2019-07-07 17:22:41 +0200

    Archivos esenciales que olvidé incluir >:(

---
+++ nopentium.c
@@ -0,0 +1,15 @@
+#include "visor/visor.hpp"
+#include <stdlib.h>
+
+buffer_sprite3d *visor_init(lptvisor datos_render)
+{
+    return NULL;
+}
+
+void visor_loop(lptvisor datos_render)
+{
+}
+
+void visor_end()
+{
+}
