9195b33ce04de9be976ace2170d3e209506c0bdd
vii <vii@divero.net>
2020-10-09 17:30:45 +0200

    cacheando watcom

--- .travis/install_watcom.sh
+++ .travis/install_watcom.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-case $TRAVIS_OS_NAME in
-    linux)
-        wget https://nav.dl.sourceforge.net/project/openwatcom/open-watcom-1.9/open-watcom-c-linux-1.9 -O owatcom.zip || wget ftp://ftp.openwatcom.org/install/open-watcom-c-linux-1.9 -O owatcom.zip
-        mkdir ~/watcom
-        unzip owatcom.zip -d ~/watcom/
-        pushd ~/watcom/binl
-        chmod a+x ctags dmpobj edbind exe2bin fcenable ide2make ms2wlink owcc parserv par.trp std.trp tcpserv tcp.trp vi wasaxp wasm wasppc wbind wcc wcc386 wcl wcl386 wd wdis wdump whelp wlib wlink wmake wpp wpp386 wrc wstrip wtouch
-        popd
-        wcl386 -v
-        ;;
-    windows)
-        wget https://nav.dl.sourceforge.net/project/openwatcom/open-watcom-1.9/open-watcom-c-win32-1.9.exe -O owatcom.zip || wget ftp://ftp.openwatcom.org/install/open-watcom-c-win32-1.9.exe -O owatcom.zip
-        mkdir ~/watcom
-        unzip owatcom.zip -d ~/watcom/
-        wcl386 -v
-        ;;
-esac
+if [ ! -d ~/watcom ]; then
+    echo Installing Open Watcom...
+    case $TRAVIS_OS_NAME in
+        linux)
+            wget https://nav.dl.sourceforge.net/project/openwatcom/open-watcom-1.9/open-watcom-c-linux-1.9 -O owatcom.zip || wget ftp://ftp.openwatcom.org/install/open-watcom-c-linux-1.9 -O owatcom.zip
+            mkdir ~/watcom
+            unzip owatcom.zip -d ~/watcom/
+            pushd ~/watcom/binl
+            chmod a+x ctags dmpobj edbind exe2bin fcenable ide2make ms2wlink owcc parserv par.trp std.trp tcpserv tcp.trp vi wasaxp wasm wasppc wbind wcc wcc386 wcl wcl386 wd wdis wdump whelp wlib wlink wmake wpp wpp386 wrc wstrip wtouch
+            popd
+            wcl386 -v
+            ;;
+        windows)
+            wget https://nav.dl.sourceforge.net/project/openwatcom/open-watcom-1.9/open-watcom-c-win32-1.9.exe -O owatcom.zip || wget ftp://ftp.openwatcom.org/install/open-watcom-c-win32-1.9.exe -O owatcom.zip
+            mkdir ~/watcom
+            unzip owatcom.zip -d ~/watcom/
+            wcl386 -v
+            ;;
+    esac
+else
+    echo Open Watcom already installed!
+fi
