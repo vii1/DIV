b3f37414abb7b70fb87e21e49f511f31797f315d
vii <vii@divero.net>
2020-10-17 20:38:32 +0200

    wip: install

--- src/install/main.c
+++ src/install/main.c
@@ -3,19 +3,28 @@
 #include <string.h>
 #include <ctype.h>
 
+char install_dir[_MAX_PATH + 1];
+
+void chdir_to_install_dir() {
+	int	 n;
+	_fullpath( install_dir, argv[0], _MAX_PATH + 1 );
+	n = strlen( install_dir );
+	while( n && install_dir[n] != '\\' ) n--;
+	install_dir[n] = 0;
+	if( install_dir[n - 1] == ':' ) strcat( install_dir, "\\" );
+	_dos_setdrive( (int)toupper( install_dir[0] ) - 'A' + 1, &n );
+	chdir( install_dir );
+}
+
 int main( int argc, char* argv[] )
 {
-	char full[_MAX_PATH + 1];
-	int	 n;
 
 	if( argc < 1 ) return 0;
-	_fullpath( full, argv[0], _MAX_PATH + 1 );
-	n = strlen( full );
-	while( n && full[n] != '\\' ) n--;
-	full[n] = 0;
-	if( full[n - 1] == ':' ) strcat( full, "\\" );
-	_dos_setdrive( (int)toupper( full[0] ) - 'A' + 1, &n );
-	chdir( full );
     
+	// TODO: considerar un handler que sirva para algo
+	//  _harderr(critical_error);
+
+	chdir_to_install_dir();
+
 	return 0;
 }
