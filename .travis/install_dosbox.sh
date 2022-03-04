476f3778e9619c15a8dae2bb79df307dd7819290
vii <vii@divero.net>
2022-03-04 21:13:10 +0100

    Fix dosbox build in linux

--- .travis/install_dosbox.sh
+++ .travis/install_dosbox.sh
@@ -3,16 +3,20 @@ if [ ! -f $DOSBOX ]; then
     echo Installing Dosbox-X...
     case $TRAVIS_OS_NAME in
         linux)
-            sudo apt-get -y install build-essential automake autoconf libsdl2-dev libsdl2-net-dev
-            wget https://github.com/joncampbell123/dosbox-x/archive/refs/tags/dosbox-x-v0.83.23.tar.gz
-            tar -xzf dosbox-x-v0.83.23.tar.gz
-            cd dosbox-x-dosbox-x-v0.83.23
-            ./autogen.sh
+            sudo apt-get -y update || exit 1
+            sudo apt-get -y install build-essential automake autoconf libsdl2-dev libsdl2-net-dev || exit 1
+            wget https://github.com/joncampbell123/dosbox-x/archive/refs/tags/dosbox-x-v0.83.23.tar.gz || exit 1
+            tar -xzf dosbox-x-v0.83.23.tar.gz || exit 1
+            cd dosbox-x-dosbox-x-v0.83.23 || exit 1
+            ./autogen.sh || exit 1
             chmod +x vs/sdl/build-scripts/strip_fPIC.sh
             chmod +x configure
-            ./configure --enable-core-inline --disable-debug --disable-x11 --disable-sdl2test --disable-sdltest --disable-alsatest --disable-freetype --disable-printer --disable-mt32 --disable-screenshots --disable-libslirp --disable-libfluidsynth --disable-opengl --disable-sdl --enable-sdl2 --prefix=$HOME/dosbox-x
-            make -j`nproc`
-            make install
+            ./configure --enable-core-inline --disable-debug --disable-x11 --disable-sdl2test \
+                --disable-sdltest --disable-alsatest --disable-freetype --disable-printer --disable-mt32 \
+                --disable-screenshots --disable-libslirp --disable-libfluidsynth --disable-opengl --disable-sdl \
+                --enable-sdl2 --prefix=$HOME/dosbox-x || exit 1
+            make -j`nproc` || exit 1
+            make install || exit 1
             ;;
         windows)
             wget https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.23/dosbox-x-vsbuild-win64-20220228210811.zip -O dosbox-x.zip
