e60146fd47f6e13cf897f61fa09b76cdc7112392
vii <vii@divero.net>
2022-03-04 22:21:28 +0100

    use scoop

--- .travis/install_dosbox.sh
+++ .travis/install_dosbox.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+case $TRAVIS_OS_NAME in
+	linux)
+		sudo apt-get -y update || exit 1
+		sudo apt-get -y install libsdl2-dev libsdl2-net-dev || exit 1
+		;;
+esac
 if [ ! -f $DOSBOX ]; then
     echo Installing Dosbox-X...
     case $TRAVIS_OS_NAME in
         linux)
-            sudo apt-get -y update || exit 1
-            sudo apt-get -y install build-essential automake autoconf libsdl2-dev libsdl2-net-dev || exit 1
+            sudo apt-get -y install build-essential automake autoconf || exit 1
             wget https://github.com/joncampbell123/dosbox-x/archive/refs/tags/dosbox-x-v0.83.23.tar.gz || exit 1
             tar -xzf dosbox-x-v0.83.23.tar.gz || exit 1
             cd dosbox-x-dosbox-x-v0.83.23 || exit 1
@@ -20,10 +25,12 @@ if [ ! -f $DOSBOX ]; then
             cd ..
             ;;
         windows)
-            wget https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.23/dosbox-x-vsbuild-win64-20220228210811.zip -O dosbox-x.zip
-            unzip dosbox-x.zip -d dosbox_tmp
-            mv dosbox_tmp/bin/x64/Release\ SDL2 ./dosbox-x
-            rm -fR dosbox_tmp
+            # https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.23/dosbox-x-mingw-win64-20220228123456.zip -O dosbox-x.zip
+            # unzip dosbox-x.zip -d dosbox_tmp
+            # mv dosbox_tmp/mingw-build/mingw-sdl2 ./dosbox-x
+            # rm -fR dosbox_tmp
+			powershell -Command iwr -useb get.scoop.sh \| iex
+			scoop update && scoop install dosbox-x
             ;;
     esac
 else
